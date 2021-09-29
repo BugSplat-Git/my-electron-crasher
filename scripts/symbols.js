84d11e972598aaaf49febb6b43fb7c96cc4ca07d
Bobby Galli <bobbyg603@gmail.com>
2021-09-29 10:10:28 -0400

    Updates @bugsplat/symbol-upload to 3.0.0

--- scripts/symbols.js
+++ scripts/symbols.js
@@ -1,6 +1,7 @@
 const { BugSplatApiClient, SymbolsApiClient } = require('@bugsplat/symbol-upload');
 const packageJson = require('../package.json');
 const fs = require('fs');
+const path = require('path');
 
 const email = process.env.SYMBOL_UPLOAD_EMAIL;
 if (!email) {
@@ -19,17 +20,27 @@ const version = packageJson.version;
 const buildDirectory = `./dist/src`;
 const files = fs.readdirSync(buildDirectory)
     .filter(file => file.endsWith('.js.map'))
-    .map(file => (`${buildDirectory}/${file}`));
+    .map(file => {
+        const filePath = `${buildDirectory}/${file}`;
+        const stat = fs.statSync(filePath);
+        const name = path.basename(filePath);
+        const size = stat.size;
+        return {
+            name,
+            size,
+            file: fs.createReadStream(filePath)
+        };
+    });
 
 const bugsplat = new BugSplatApiClient();
 const symbolsApiClient = new SymbolsApiClient(bugsplat);
 return bugsplat.login(email, password)
-    .then(() => symbolsApiClient.delete(
+    .then(() => symbolsApiClient.deleteSymbols(
         database,
         application,
         version
     ))
-    .then(() => symbolsApiClient.post(
+    .then(() => symbolsApiClient.postSymbols(
         database,
         application,
         version,
