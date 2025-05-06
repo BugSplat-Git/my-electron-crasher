9bb7709aef7292ac8a6c89f46e4f4fc42a020a74
Bobby Galli <bobbyg603@gmail.com>
2025-05-06 09:57:23 -0400

    chore: create codeql.yml (#265)
    * chore: create codeql.yml
    
    * chore: update codeql
    
    * chore: update .github/workflows/codeql.yml
    
    * fix: improve sample c++
    
    * chore: fix workflow copilot broke
    
    ---------
    
    Co-authored-by: BugSplatService <support@bugsplat.com>

--- src/plugin/example.cpp
+++ src/plugin/example.cpp
@@ -2,30 +2,33 @@
 
 using namespace std;
 
-int example::add(int x, int y) {
+int example::add(int x, int y)
+{
+    int *p = NULL;
+    *p = 100;
     return (x + y);
 }
 
-Napi::Number example::addWrapped(const Napi::CallbackInfo &info) {
-    int *p = NULL;
-    *p = 100;
-
+Napi::Number example::addWrapped(const Napi::CallbackInfo &info)
+{
     Napi::Env env = info.Env();
-    //check if arguments are integer only.
-    if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber()) {
+    // Check if arguments are integer only.
+    if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber())
+    {
         Napi::TypeError::New(env, "arg1::Number, arg2::Number expected").ThrowAsJavaScriptException();
     }
-    //convert javascripts datatype to c++
+    // Convert javascripts datatype to c++
     Napi::Number first = info[0].As<Napi::Number>();
     Napi::Number second = info[1].As<Napi::Number>();
-//run c++ function return value and return it in javascript
+    // Run c++ function return value and return it in javascript
     Napi::Number returnValue = Napi::Number::New(env, example::add(first.Int32Value(), second.Int32Value()));
 
     return returnValue;
 }
 
-Napi::Object example::Init(Napi::Env env, Napi::Object exports) {
-    //export Napi function
+Napi::Object example::Init(Napi::Env env, Napi::Object exports)
+{
+    // Export Napi function
     exports.Set("add", Napi::Function::New(env, example::addWrapped));
     return exports;
 }
